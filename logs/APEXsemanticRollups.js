9b9d9e78044f48a98dd86af60b8dc79adf5ed6fe
Sean Muir <sean.muir@jkmsoftware.com>
2023-04-14 10:00:00 -0400

    updates to version
    Removed unit test and etc

--- logs/APEXsemanticRollups.js
+++
@@ -1,8 +0,0 @@
-try {
- load("nashorn:mozilla_compat.js");
-} 
-catch (e) 
-{ 
-}
-importPackage(org.mdmi.core.engine.javascript);
-function EncounterStatus_COMPUTED(value) {value.getXValue().addValue('code', 'finished');}function GenderIdentity_to_GenderIdentityRollupRollUp(value,param1) {value.getXValue().addValue('REPLACEWITHELEMENT', '<<<LOCALSEMANTICVALUE>>>' );}function Age_to_AgerollupRollUp(value,param1) {value.getXValue().addValue('REPLACEWITHELEMENT', '<<<LOCALSEMANTICVALUE>>>' );}function fIRSTNAME_to_PatientNameRollupRollUp(value,param1) {value.getXValue().addValue('given', param1 );}function LastNAME_to_PatientNameRollupRollUp(value,param1) {value.getXValue().addValue('family', param1 );}
