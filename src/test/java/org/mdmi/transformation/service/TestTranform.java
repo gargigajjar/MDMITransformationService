ab6e635f941521255a534587eed276ce0d878931
gargigajjar <ggajjar@mdixinc.com>
2024-09-12 14:31:22 -0400

    Updated maps and xsd for loopF

--- src/test/java/org/mdmi/transformation/service/TestTranform.java
+++ src/test/java/org/mdmi/transformation/service/TestTranform.java
@@ -36,7 +36,7 @@ class TestTransform {
 
 	private static byte[] readInputMessage() {
 		try {
-			return StreamUtils.readStream(new FileInputStream("full-input-message.edi"));
+			return StreamUtils.readStream(new FileInputStream("LoopF.edi"));
 		} catch (IOException e) {
 			e.printStackTrace();
 			return "<no-message/>".getBytes();
